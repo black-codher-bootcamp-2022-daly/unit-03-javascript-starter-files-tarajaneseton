02bfddf10a175137f24ff23c8b77e716d5675267
tarajaneseton <tarajaneseton@gmail.com>
2022-10-10 19:07:30 +0100

    let a=20

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,18 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+let a = 5;
+const b = 10;
+
+let c = a + b;
+
+console.log (c);
+
+a = 20;
+
+c = a + b;
+
+console.log(c);
+
+
+
+
