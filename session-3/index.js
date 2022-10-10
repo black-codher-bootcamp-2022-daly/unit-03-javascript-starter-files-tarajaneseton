3645f58edbf241fdc9d89199e4328036c6ebec80
tarajaneseton <tarajaneseton@gmail.com>
2022-10-10 21:54:21 +0100

    function name + age in 5 years

--- session-3/index.js
+++ session-3/index.js
@@ -1,18 +1,9 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
-let a = 5;
-const b = 10;
+function futureAge (name, age) {
+    const calculateAge = age + 5
+    return "Hi" + name + "!" + calculateAge
+}
+futureAge ( "tara", 35 );
 
-let c = a + b;
-
-console.log (c);
-
-a = 20;
-
-c = a + b;
-
-console.log(c);
-
-
-
-
+console.log(futureAge("Tara", 35))
