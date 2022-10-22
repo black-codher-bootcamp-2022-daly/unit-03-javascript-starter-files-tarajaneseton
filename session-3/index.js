3b8aa31226e534ae3ee6b005c212778a2bbba81a
tarajaneseton <tarajaneseton@gmail.com>
2022-10-22 11:49:19 +0100

    conversation function

--- session-3/index.js
+++ session-3/index.js
@@ -1,9 +1,26 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
-function futureAge (name, age) {
-    const calculateAge = age + 5
-    return "Hi" + name + "!" + calculateAge
-}
-futureAge ( "tara", 35 );
 
-console.log(futureAge("Tara", 35))
+function sayHey() { 
+    console.log("Hey!"); 
+}
+
+function conversation() {
+    sayHey(); 
+    console.log("How are you?");
+    console.log("Goodbye"); 
+}
+
+conversation();
+
+// sayHey();
+
+
+
+// function futureAge (name, age) {
+//     const calculateAge = age + 5
+//     return "Hi" + name + "!" + calculateAge
+// }
+// futureAge ( "tara", 35 );
+
+// console.log(futureAge("Tara", 35))
