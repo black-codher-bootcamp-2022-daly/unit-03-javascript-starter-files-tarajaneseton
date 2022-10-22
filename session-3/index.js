f94385d9ca2093692f80484d2fe6b5ce71a59648
tarajaneseton <tarajaneseton@gmail.com>
2022-10-22 21:43:34 +0100

    task 2 conversation

--- session-3/index.js
+++ session-3/index.js
@@ -1,30 +1,30 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
 
-let a = 5;
-const b = 10;
-let c = a + b;
+// let a = 5;
+// const b = 10;
+// let c = a + b;
 
-console.log(c);
+// console.log(c);
 
-a = 20;
+// a = 20;
 
-console.log(a + b);
+// console.log(a + b);
 
-c = a + b;
+// c = a + b;
 
-console.log(c);
+// console.log(c);
 
-// function sayHey() { 
-//     console.log("Hey!"); 
-// }
+function sayHey() { 
+    console.log("Hey!"); 
+}
 
-// function conversation() {
-//     sayHey(); 
-//     console.log("How are you?");
-//     console.log("Goodbye"); 
-// }
+function conversation(name, topic) {
+    sayHey(name); 
+    console.log("Do you like" + topic + "?");
+}
 
+conversation("Erika", "pizza");
 // conversation();
 
 // // sayHey();
