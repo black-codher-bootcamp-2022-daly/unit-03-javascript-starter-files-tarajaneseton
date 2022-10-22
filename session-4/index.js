a35e2200d0b76054973ed5963818b5a4611d3ef6
tarajaneseton <tarajaneseton@gmail.com>
2022-10-22 22:14:59 +0100

    calculator homework

--- session-4/index.js
+++ session-4/index.js
@@ -2,36 +2,60 @@
     
     
 
-const personA = {
-    name: 'Will',
-    age: 34,
-    location: 'east london',
-    likes: 'music'
+// const personA = {
+//     name: 'Will',
+//     age: 34,
+//     location: 'east london',
+//     likes: 'music'
+// }
+
+// const personB = {
+//     name: 'cleo',
+//     age: 2,
+//     location: 'east london',
+//     likes: 'apples'
+// }
+
+// const personC = {
+//     name: 'cherelle',
+//     age: 34,
+//     location: 'croydon',
+//     likes: 'wine'
+// }
+
+// const personD = {
+//     name: 'Tara',
+//     age: 34,
+//     location: 'east london',
+//     likes: 'chocolate'
+// }
+
+// function biography(person) {
+//     return `Hi my name is ${person.name}, I am ${person.age} years old and I live in ${person.location}. I like ${person.likes}.`
+//     }
+
+// console.log(biography(personC));
+
+
+
+
+
+function addNumbers (x,y) {
+    return x + y
 }
 
-const personB = {
-    name: 'cleo',
-    age: 2,
-    location: 'east london',
-    likes: 'apples'
+function subtractNumbers(x,y) {
+    return x - y
 }
 
-const personC = {
-    name: 'cherelle',
-    age: 34,
-    location: 'croydon',
-    likes: 'wine'
+const calculator = {
+    add: addNumbers,
+    subtract: subtractNumbers,
+    multiply: function multiplyNumbers(x,y) {
+        return x * y
+    },
+    divide: (x, y) => x / y,
 }
 
-const personD = {
-    name: 'Tara',
-    age: 34,
-    location: 'east london',
-    likes: 'chocolate'
-}
 
-function biography(person) {
-    return `Hi my name is ${person.name}, I am ${person.age} years old and I live in ${person.location}. I like ${person.likes}.`
-    }
-
-console.log(biography(personC));
+console.log(calculator.divide(10,2))
