a6857582f0e5b4687065dd159ea5bc1482f0c3e1
tarajaneseton <tarajaneseton@gmail.com>
2022-10-22 11:33:39 +0100

    display inner html and about element

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,3 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log( document.querySelector( '.about').innerHTML);
--- session-6/index.js
+++ session-6/index.js
@@ -3,16 +3,26 @@
 
 // ****************************************************TASK 1****************************************************
 
-let counter = 0
+// let counter = 0
 
 
-while (counter <= 10) {
+// while (counter < 10) {
+//     const randomNumber = Math.round(Math.random() * 10); 
+//         console.log('while random', randomNumber)
+//         counter++
+//     }
+
+
+
+let counter = 1
+let randomNumber = 1
+
+
+for ( let counter = 1; counter <= 10; counter++) {
     const randomNumber = Math.round(Math.random() * 10); 
-        console.log('while random', randomNumber)
-        counter++
-    }
-
-
+    console.log('while random', randomNumber)
+}
+        
 
 
 
