74ac54e0ad7c638d70170ce4edb758f0fbf77db4
tarajaneseton <tarajaneseton@gmail.com>
2022-10-22 21:31:29 +0100

    changed var to let

--- session-3/index.js
+++ session-3/index.js
@@ -1,19 +1,33 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
 
-function sayHey() { 
-    console.log("Hey!"); 
-}
+let a = 5;
+const b = 10;
+let c = a + b;
 
-function conversation() {
-    sayHey(); 
-    console.log("How are you?");
-    console.log("Goodbye"); 
-}
+console.log(c);
 
-conversation();
+a = 20;
 
-// sayHey();
+console.log(a + b);
+
+c = a + b;
+
+console.log(c);
+
+// function sayHey() { 
+//     console.log("Hey!"); 
+// }
+
+// function conversation() {
+//     sayHey(); 
+//     console.log("How are you?");
+//     console.log("Goodbye"); 
+// }
+
+// conversation();
+
+// // sayHey();
 
 
 
